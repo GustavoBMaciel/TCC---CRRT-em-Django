e9aa52f97a19b0e0c2da437a7ce99d2881d62e6f
GustavoBMaciel <gustavo.barros.maciel@gmail.com>
2018-08-05 21:55:50 -0300

    ERP em Django

--- crrt/admin.py
+++ crrt/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+class ProdutoAdmin (admin.ModelAdmin):
+    list_display = ('nome', 'imagem', 'quantidade', 'data_criado')
 
 # Register your models here.
+admin.site.register (models.Produto, ProdutoAdmin)
+admin.site.register (models.Cliente)
+admin.site.register (models.Venda)
--- crrt/models.py
+++ crrt/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Produto (models.Model):
+    nome = models.CharField(max_length=200)
+    quantidade = models.IntegerField (default=0)
+    ativo = models.BooleanField (default=False)
+    imagem = models.ImageField (null=True,blank=True,upload_to='galeria')
+    descricao = models.TextField()
+    data_criado = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.nome
+        
+class Cliente (models.Model):    
+    cnpj = models.CharField(max_length=14)
+    inscricaoestadual = models.CharField(max_length=12)
+    nome = models.CharField(max_length=200)
+    telefone = models.IntegerField (default=0)
+    endereco = models.CharField(max_length=200)
+    numero = models.IntegerField (default=0)
+    bairro = models.CharField(max_length=50)
+    complemento = models.CharField(max_length=50)
+    ativo = models.BooleanField (default=False)
+    email = models.EmailField()
+    descricao = models.TextField(max_length=500)
+    data_criado = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.nome
+
+class Venda (models.Model):
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE)
+    produto = models.ManyToManyField(Produto)
+    data_criado = models.DateTimeField(auto_now_add=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
